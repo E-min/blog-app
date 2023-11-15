04c573a2db32d9d2428ec08a10db9ca64addb8a3
Mehmet Emin <eminkocabuga@gmail.com>
2023-11-15 23:18:30 +0300

    changes

--- src/components/pages/MyBlogs/MyBlogs.jsx
+++ src/components/pages/MyBlogs/MyBlogs.jsx
@@ -68,7 +68,6 @@ const MyBlogs = () => {
             alignItems: "center",
             width: "100%",
             height: "100%",
-            border: "2px solid #000",
           }}
         >
           <motion.div
